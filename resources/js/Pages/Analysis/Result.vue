7241cfda28552a3691967e7091ed2c17f31ac966
Piotr Szulc <piotrszulc@MacBook-Pro-Piotr.local>
2026-01-14 12:19:44 +0100

    Fix error with Vue

--- resources/js/Pages/Analysis/Result.vue
+++ resources/js/Pages/Analysis/Result.vue
@@ -4,6 +4,15 @@ import { router } from '@inertiajs/vue3'
 
 const loading = ref(false)
 
+// ⚠️ UWAGA: analysisId musi istnieć
+// np. jako prop:
+defineProps({
+    analysisId: {
+        type: [String, Number],
+        required: true,
+    },
+})
+
 function startPayment() {
     if (loading.value) return
     loading.value = true
@@ -12,12 +21,14 @@ function startPayment() {
 }
 </script>
 
-<button
-    class="btn-primary w-full"
-    :class="{ 'opacity-50 cursor-not-allowed': loading }"
-    :disabled="loading"
-    @click="startPayment"
->
-<span v-if="!loading">Generuj raport »</span>
-<span v-else>Przekierowanie do płatności…</span>
-</button>
+<template>
+    <button
+        class="btn-primary w-full"
+        :class="{ 'opacity-50 cursor-not-allowed': loading }"
+        :disabled="loading"
+        @click="startPayment"
+    >
+        <span v-if="!loading">Generuj raport »</span>
+        <span v-else>Przekierowanie do płatności…</span>
+    </button>
+</template>
