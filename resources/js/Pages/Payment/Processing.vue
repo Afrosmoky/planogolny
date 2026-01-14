53adc53e340e2ac262eb44bca97b0d7ecb442bdc
Piotr Szulc <piotrszulc@MacBook-Pro-Piotr.local>
2026-01-14 13:13:28 +0100

    Add processing page

---
+++ resources/js/Pages/Payment/Processing.vue
@@ -0,0 +1,33 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+defineOptions({ layout: AppLayout })
+
+defineProps({
+    results: Object
+})
+</script>
+
+<template>
+    <section class="wrapper pb-14 pb-md-16">
+        <div class="container text-center">
+            <div class="row pb-10">
+                <div
+                    class="col-lg-7 col-xxl-6 mx-auto text-center"
+
+                >
+                    <div
+                        class="d-flex justify-content-center mb-5 mb-md-0"
+
+                    >
+                        <div class="btn_top">
+                            <h3>
+                                Twoja płatność jest w trakcie realizacji
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
