0a6c18b94bffd992014b551bfa7856ae80273ece
Piotr Szulc <piotrszulc@MacBook-Pro-Piotr.local>
2026-01-24 13:45:54 +0100

    Fix webhook returns

--- packages/Payments/src/routes/webhooks.php
+++ packages/Payments/src/routes/webhooks.php
@@ -6,11 +6,17 @@ use Planogolny\Payments\Actions\HandleTpayWebhookAction;
 use Planogolny\Payments\DTO\TpayWebhookDTO;
 
 Route::post('api/webhooks/tpay', function (Request $request) {
-    app(HandleTpayWebhookAction::class)->execute(
+    $result = app(HandleTpayWebhookAction::class)->execute(
         TpayWebhookDTO::fromRequest($request)
     );
 
-    return response(TRUE, 200);
+    if ($result === true) {
+        return response('TRUE', 200)
+            ->header('Content-Type', 'text/plain');
+    }
+
+    return response('FALSE', 200)
+        ->header('Content-Type', 'text/plain');
 })->name('tpay.webhook');
 //Route::post('api/webhooks/tpay', function () {
 //    return response()->json(['webhook' => 'ok']);
